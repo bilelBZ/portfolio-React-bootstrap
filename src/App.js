927ffa838b8911d40f3eefa73c6127f4c83071b5
bilelBZ <bilelbz155@gmail.com>
2021-11-17 20:47:49 +0000

    portfolio react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>hello react...</h1>
+    </div>
+  )
+}
+
+export default App
